d55fdf629dce52305210cc573435aea6403d6936
bharatak <bharatak@thoughtworks.com>
2016-06-28 09:17:18 +0530

    Bharat| Code Cleanup

--- endtb/src/main/java/org/bahmni/test/page/registration/domain/EndtbPatient.java
+++ endtb/src/main/java/org/bahmni/test/page/registration/domain/EndtbPatient.java
@@ -6,10 +6,6 @@ public class EndTBPatient extends Patient{
 
 	private String nationalIdentificationNumber;
 
-	public EndTBPatient(String idNumber, String firstName, String lastName, String gender, Date dateOfBirth, int age) {
-		super(idNumber, firstName, lastName, gender, dateOfBirth, age);
-	}
-
 	public EndTBPatient(String idNumber, String firstName, String lastName, String gender, Date dateOfBirth, int age, String nationalIdentificationNumber) {
 		super(idNumber, firstName, lastName, gender, dateOfBirth, age);
 		this.nationalIdentificationNumber = nationalIdentificationNumber;
