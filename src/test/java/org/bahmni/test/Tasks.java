07631e541bffd28d77316ad08e437b21ae9b8f01
kumarvastav <kumar.vastav@gmail.com>
2016-07-08 14:11:08 +0530

    Dharam: Added test scenarios for create and search in core with current structure

---
+++ src/test/java/org/bahmni/test/Tasks.java
@@ -0,0 +1,15 @@
+package org.bahmni.test;
+
+import com.thoughtworks.gauge.Step;
+
+public class Tasks {
+
+    @Step({"Navigate to dashboard"})
+    public void navigateToDashboard() throws InterruptedException {
+    }
+
+    @Step("Navigate to Registration search page")
+    public void navigateToRegistrationSearchPage() throws InterruptedException {
+    }
+
+}
